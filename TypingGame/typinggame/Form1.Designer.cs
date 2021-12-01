76680cf53b79739cfd46f6425ea454ab7b605a94
vinston-dev <72871208+vinston-dev@users.noreply.github.com>
2021-12-02 00:26:00 +0100

    Add files via upload

---
+++ TypingGame/typinggame/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace typinggame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblright = new System.Windows.Forms.Label();
+            this.lblwrong = new System.Windows.Forms.Label();
+            this.lblword = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Timer1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.lblacc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblright
+            // 
+            this.lblright.AutoSize = true;
+            this.lblright.Location = new System.Drawing.Point(198, 29);
+            this.lblright.Name = "lblright";
+            this.lblright.Size = new System.Drawing.Size(53, 13);
+            this.lblright.TabIndex = 0;
+            this.lblright.Text = "Correct: 0";
+            // 
+            // lblwrong
+            // 
+            this.lblwrong.AutoSize = true;
+            this.lblwrong.Location = new System.Drawing.Point(687, 29);
+            this.lblwrong.Name = "lblwrong";
+            this.lblwrong.Size = new System.Drawing.Size(61, 13);
+            this.lblwrong.TabIndex = 1;
+            this.lblwrong.Text = "Incorrect: 0";
+            // 
+            // lblword
+            // 
+            this.lblword.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblword.AutoSize = true;
+            this.lblword.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblword.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblword.Location = new System.Drawing.Point(433, 115);
+            this.lblword.Name = "lblword";
+            this.lblword.Size = new System.Drawing.Size(111, 42);
+            this.lblword.TabIndex = 2;
+            this.lblword.Text = "Word";
+            this.lblword.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(403, 299);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(178, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Press space to shift to the next word";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(342, 198);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(318, 49);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.CheckGame);
+            // 
+            // Timer1
+            // 
+            this.Timer1.AutoSize = true;
+            this.Timer1.Font = new System.Drawing.Font("Poor Richard", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Timer1.Location = new System.Drawing.Point(447, 55);
+            this.Timer1.Name = "Timer1";
+            this.Timer1.Size = new System.Drawing.Size(68, 22);
+            this.Timer1.TabIndex = 5;
+            this.Timer1.Text = " Wpm: 0";
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 10;
+            this.timer2.Tick += new System.EventHandler(this.Timer2_Tick);
+            // 
+            // lblacc
+            // 
+            this.lblacc.AutoSize = true;
+            this.lblacc.Location = new System.Drawing.Point(403, 9);
+            this.lblacc.Name = "lblacc";
+            this.lblacc.Size = new System.Drawing.Size(75, 13);
+            this.lblacc.TabIndex = 6;
+            this.lblacc.Text = "Accuracy: 0 %";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1035, 472);
+            this.Controls.Add(this.lblacc);
+            this.Controls.Add(this.Timer1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblword);
+            this.Controls.Add(this.lblwrong);
+            this.Controls.Add(this.lblright);
+            this.Name = "Form1";
+            this.Text = "typing game test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblright;
+        private System.Windows.Forms.Label lblwrong;
+        private System.Windows.Forms.Label lblword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label Timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label lblacc;
+    }
+}
+
